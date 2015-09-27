0ec8cfe132dac12c5e2c72623011c81c28dc1775
Daniel Häfele <haefele@xemio.net>
2015-09-27 12:25:36 +0200

    Add DayStartTime and DayEndTime to our TimesByDay class

--- src/CTime2/Views/YourTimes/TimesByDay.cs
+++ src/CTime2/Views/YourTimes/TimesByDay.cs
@@ -12,12 +12,20 @@ namespace CTime2.Views.YourTimes
         public DateTime Day { get; }
         public TimeSpan? Hours { get; }
         public BindableCollection<TimeForGrouping> Times { get; }
+        public TimeSpan? DayStartTime { get; }
+        public TimeSpan? DayEndTime { get; }
 
         private TimesByDay(DateTime day, BindableCollection<TimeForGrouping> times)
         {
             this.Day = day;
             this.Times = times;
             this.Hours = times.Select(f => f.Time.Hours).FirstOrDefault();
+
+            var clockInTimes = times.Where(f => f.ClockInTime != null).Select(f => f.ClockInTime.Value.TimeOfDay).ToList();
+            this.DayStartTime = clockInTimes.Any() ? clockInTimes.Min() : (TimeSpan?)null;
+
+            var clockOutTimes = times.Where(f => f.ClockOutTime != null).Select(f => f.ClockOutTime.Value.TimeOfDay).ToList();
+            this.DayEndTime = clockOutTimes.Any() ? clockOutTimes.Max() : (TimeSpan?)null;
         }
 
         public static IEnumerable<TimesByDay> Create(IEnumerable<Time> times)
