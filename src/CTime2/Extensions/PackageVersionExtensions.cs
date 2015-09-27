489ac04c001b64e22eea70fcd7d778859abcba50
Daniel Häfele <haefele@xemio.net>
2015-09-27 18:07:05 +0200

    Improve the about-view
    This is for issue #5.

---
+++ src/CTime2/Extensions/PackageVersionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Windows.ApplicationModel;
+
+namespace CTime2.Extensions
+{
+    public static class PackageVersionExtensions
+    {
+        public static Version ToVersion(this PackageVersion self)
+        {
+            return new Version(self.Major, self.Minor, self.Build, self.Revision);
+        }
+    }
+}
--- src/CTime2/Views/About/AboutViewModel.cs
+++ src/CTime2/Views/About/AboutViewModel.cs
@@ -1,12 +1,29 @@
-﻿using Caliburn.Micro;
+﻿using System;
+using Windows.ApplicationModel;
+using Caliburn.Micro;
+using CTime2.Extensions;
 
 namespace CTime2.Views.About
 {
     public class AboutViewModel : Screen
     {
+        private Version _currentVersion;
+
+        public Version CurrentVersion
+        {
+            get { return this._currentVersion; }
+            set { this.SetProperty(ref this._currentVersion, value); }
+        }
+
         public AboutViewModel()
         {
             this.DisplayName = "Über";
+
+            this.CurrentVersion = Package.Current.Id.Version.ToVersion();
+        }
+        
+        public void ShowUsedSoftware()
+        {
         }
     }
 }
