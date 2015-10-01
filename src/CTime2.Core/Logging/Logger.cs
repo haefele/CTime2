e56e9ee67c22a79864b65e79121b4cc8e7ad2896
Daniel Häfele <haefele@xemio.net>
2015-10-01 22:22:28 +0200

    Add logging basics for Anotar.Fody
    Currently Anotar.Fody isn't working for UWP projects, but for now I have prepared everything to use it once it works.

---
+++ src/CTime2.Core/Logging/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MetroLog;
+
+namespace CTime2.Core.Logging
+{
+    public class Logger
+    {
+        private readonly ILogger _logger;
+
+        public Logger(ILogger logger)
+        {
+            this._logger = logger;
+        }
+
+        public void Debug(string format, params object[] args) => this._logger.Debug(format, args);
+        public void Debug(Exception exception, string format, params object[] args) => this._logger.Debug(string.Format(format, args), exception);
+        public bool IsDebugEnabled => this._logger.IsDebugEnabled;
+        public void Information(string format, params object[] args) => this._logger.Info(format, args);
+        public void Information(Exception exception, string format, params object[] args) => this._logger.Info(string.Format(format, args), exception);
+        public bool IsInformationEnabled => this._logger.IsInfoEnabled;
+        public void Warning(string format, params object[] args) => this._logger.Warn(format, args);
+        public void Warning(Exception exception, string format, params object[] args) => this._logger.Warn(string.Format(format, args), exception);
+        public bool IsWarningEnabled => this._logger.IsWarnEnabled;
+        public void Error(string format, params object[] args) => this._logger.Error(format, args);
+        public void Error(Exception exception, string format, params object[] args) => this._logger.Error(string.Format(format, args), exception);
+        public bool IsErrorEnabled => this._logger.IsErrorEnabled;
+        public void Fatal(string format, params object[] args) => this._logger.Fatal(format, args);
+        public void Fatal(Exception exception, string format, params object[] args) => this._logger.Fatal(string.Format(format, args), exception);
+        public bool IsFatalEnabled => this._logger.IsFatalEnabled;
+    }
+}
---
+++ src/CTime2.Core/Logging/LoggerFactory.cs
@@ -0,0 +1,17 @@
+﻿using MetroLog;
+
+namespace CTime2.Core.Logging
+{
+    public class LoggerFactory
+    {
+        static LoggerFactory()
+        {
+            //TODO: Configure MetroLog
+        }
+
+        public static Logger GetLogger<T>()
+        {
+            return new Logger(LogManagerFactory.DefaultLogManager.GetLogger<T>());
+        }
+    }
+}
