5d53e0e71413344c0b105900bc69a71895116232
Daniel Häfele <haefele@xemio.net>
2015-10-04 15:19:25 +0200

    Add translations for the exception handler
    This is for issue #26.

--- src/CTime2/Services/ExceptionHandler/ExceptionHandler.cs
+++ src/CTime2/Services/ExceptionHandler/ExceptionHandler.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using CTime2.Core.Common;
 using CTime2.Extensions;
 using CTime2.Services.Dialog;
+using CTime2.Strings;
 
 namespace CTime2.Services.ExceptionHandler
 {
@@ -14,18 +15,17 @@ namespace CTime2.Services.ExceptionHandler
         {
             this._dialogService = dialogService;
         }
-
-
+        
         public async Task HandleAsync(Exception exception)
         {
             if (exception is CTimeException)
             {
-                await this._dialogService.ShowAsync(exception.Message, "Fehler");
+                await this._dialogService.ShowAsync(exception.Message, CTime2Resources.Get("ExceptionHandler.ErrorTitle"));
             }
             else
             {
                 //TODO: Logging
-                await this._dialogService.ShowAsync("Es ist ein Fehler aufgetreten.", "Fehler");
+                await this._dialogService.ShowAsync(CTime2Resources.Get("ExceptionHandler.ErrorMessage"), CTime2Resources.Get("ExceptionHandler.ErrorTitle"));
             }
         }
     }
