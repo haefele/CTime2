977eba40e00e748ae00d6934ea16347598d07325
Daniel Häfele <haefele@xemio.net>
2016-06-14 23:03:03 +0200

    Add a StampButton control to reuse most of the stamp button layouting

---
+++ src/CTime2/Controls/StampButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace CTime2.Controls
+{
+    public class StampButton : ContentControl
+    {
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command", typeof(ICommand), typeof(StampButton), new PropertyMetadata(default(ICommand)));
+
+        public ICommand Command
+        {
+            get { return (ICommand) GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty SymbolProperty = DependencyProperty.Register(
+            "Symbol", typeof(Symbol), typeof(StampButton), new PropertyMetadata(default(Symbol)));
+
+        public Symbol Symbol
+        {
+            get { return (Symbol) GetValue(SymbolProperty); }
+            set { SetValue(SymbolProperty, value); }
+        }
+
+        
+
+        public StampButton()
+        {
+            this.DefaultStyleKey = typeof(StampButton);
+        }
+    }
+}
