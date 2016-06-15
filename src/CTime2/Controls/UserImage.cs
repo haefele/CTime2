5c1d2c60dc23fa97c81b1a308c859c8508f28951
Daniel Häfele <haefele@xemio.net>
2016-06-15 22:09:24 +0200

    Improve the user-image and allow to change the border brush

---
+++ src/CTime2/Controls/UserImage.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CTime2.Controls
+{
+    public class UserImage : Control
+    {
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(
+            "Image", typeof(byte[]), typeof(UserImage), new PropertyMetadata(default(byte[])));
+
+        public byte[] Image
+        {
+            get { return (byte[])this.GetValue(ImageProperty); }
+            set { this.SetValue(ImageProperty, value); }
+        }
+
+        public UserImage()
+        {
+            this.DefaultStyleKey = typeof(UserImage);
+        }
+    }
+}
