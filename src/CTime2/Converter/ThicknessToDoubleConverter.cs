d1e9b4d463e2b15c0f70c946abbcd3610114e027
Daniel Häfele <haefele@xemio.net>
2016-06-16 10:28:28 +0200

    Adjust colors and make the UserImage use the border-thickness property

---
+++ src/CTime2/Converter/ThicknessToDoubleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace CTime2.Converter
+{
+    public class ThicknessToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Thickness == false)
+                return value;
+
+            var thickness = (Thickness)value;
+
+            return (thickness.Left + thickness.Top + thickness.Right + thickness.Bottom)/4;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
