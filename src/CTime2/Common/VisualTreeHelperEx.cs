30d70ca48c70bfdeb8e8733588e59f4725f2d296
Daniel Häfele <haefele@xemio.net>
2016-08-11 22:17:13 +0200

    Add special background colors for saturday and sunday
    This fixes issue #54.

---
+++ src/CTime2/Common/VisualTreeHelperEx.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace CTime2.Common
+{
+    public static class VisualTreeHelperEx
+    {
+        public static T GetParent<T>(DependencyObject dependencyObject) where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(dependencyObject);
+
+            while (parent != null)
+            {
+                if (parent is T)
+                    return (T) parent;
+
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return default(T);
+        }
+    }
+}
--- src/CTime2/Views/YourTimes/YourTimesView.xaml.cs
+++ src/CTime2/Views/YourTimes/YourTimesView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using CTime2.Common;
 
 namespace CTime2.Views.YourTimes
 {
@@ -9,6 +13,39 @@ namespace CTime2.Views.YourTimes
         public YourTimesView()
         {
             this.InitializeComponent();
+        }
+
+        private void FrameworkElement_OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            this.UpdateBackground((FrameworkElement)sender);
+        }
+
+        private void FrameworkElement_OnDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            this.UpdateBackground(sender);
+        }
+
+        private void UpdateBackground(FrameworkElement frameworkElement)
+        {
+            var headerItem = VisualTreeHelperEx.GetParent<ListViewHeaderItem>(frameworkElement);
+
+            if (headerItem == null)
+                return;
+
+            var viewModel = (TimesByDay) frameworkElement.DataContext;
+
+            if (viewModel.Day.DayOfWeek == DayOfWeek.Saturday)
+            {
+                headerItem.Background = (Brush)Application.Current.Resources["CTimeLightGray"];
+            }
+            else if (viewModel.Day.DayOfWeek == DayOfWeek.Sunday)
+            {
+                headerItem.Background = (Brush)Application.Current.Resources["CTimeDarkGray"];
+            }
+            else
+            {
+                headerItem.ClearValue(BackgroundProperty);
+            }
         }
     }
 }
