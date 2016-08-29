b6f7dfc81ca93fcd41e45b8041edcde3862db706
Daniel Häfele <haefele@xemio.net>
2016-08-29 21:01:55 +0200

    When the user clicks on a chart data point we forward him to the selected day in this times history
    This is for issue #55.

--- src/CTime2/Views/Statistics/DetailedStatisticView.xaml.cs
+++ src/CTime2/Views/Statistics/DetailedStatisticView.xaml.cs
@@ -5,8 +5,11 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
+using Caliburn.Micro;
+using CTime2.Views.YourTimes;
 using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 using WinRTXamlToolkit.Controls.Extensions;
+using INavigationService = UwCore.Services.Navigation.INavigationService;
 
 namespace CTime2.Views.Statistics
 {
@@ -22,6 +25,18 @@ namespace CTime2.Views.Statistics
             this.ValueAxis.Minimum = 0;
             
             this.DateTimeAxis.IntervalType = DateTimeIntervalType.Weeks;
+        }
+
+        private void DataPointSeries_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedChartItem = (StatisticChartItem) e.AddedItems.First();
+
+            var navigationService = IoC.Get<INavigationService>();
+            navigationService
+                .For<YourTimesViewModel>()
+                .WithParam(f => f.StartDate, new DateTimeOffset(selectedChartItem.Date))
+                .WithParam(f => f.EndDate, new DateTimeOffset(selectedChartItem.Date))
+                .Navigate();
         }
     }
 }
