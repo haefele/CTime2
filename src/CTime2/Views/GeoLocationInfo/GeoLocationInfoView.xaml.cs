4f77971c939812bb108da1db61da9d56f494d3e7
Daniel Häfele <haefele@xemio.net>
2016-09-13 13:03:29 +0200

    Finish geo-location info popup
    This is for issue #76.

--- src/CTime2/Views/GeoLocationInfo/GeoLocationInfoView.xaml.cs
+++ src/CTime2/Views/GeoLocationInfo/GeoLocationInfoView.xaml.cs
@@ -1,30 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace CTime2.Views.GeoLocationInfo
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class GeoLocationInfoView : Page
     {
         public GeoLocationInfoView()
         {
             this.InitializeComponent();
+        }
+
+        private async void OpenSettingsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("ms-settings:privacy-location"));
         }
     }
 }
