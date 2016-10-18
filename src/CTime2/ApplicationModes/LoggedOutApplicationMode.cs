af78d2170bb9192440c89b83462cf8264947efc0
Daniel Häfele <haefele@xemio.net>
2016-10-18 21:58:34 +0200

    Play around how to implement a terminal
    This is for issue #100.

--- src/CTime2/ApplicationModes/LoggedOutApplicationMode.cs
+++ src/CTime2/ApplicationModes/LoggedOutApplicationMode.cs
@@ -1,4 +1,7 @@
-﻿using CTime2.Common;
+﻿using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Caliburn.Micro;
+using CTime2.Common;
 using CTime2.Strings;
 using CTime2.Views.Login;
 using UwCore.Application;
@@ -9,15 +12,18 @@ namespace CTime2.ApplicationModes
     public class LoggedOutApplicationMode : ApplicationMode
     {
         private readonly HamburgerItem _loginHamburgerItem;
+        private readonly HamburgerItem _switchToTerminalHamburgerItem;
 
         public LoggedOutApplicationMode()
         {
             this._loginHamburgerItem = new NavigatingHamburgerItem(CTime2Resources.Get("Navigation.Login"), SymbolEx.Login, typeof(LoginViewModel));
+            this._switchToTerminalHamburgerItem = new ClickableHamburgerItem("Zum Terminal", Symbol.AllApps, this.SwitchToTerminal);
         }
-        
+
         public override void Enter()
         {
             this.Application.Actions.Add(this._loginHamburgerItem);
+            this.Application.SecondaryActions.Insert(0, this._switchToTerminalHamburgerItem);
 
             this._loginHamburgerItem.Execute();
         }
@@ -25,6 +31,12 @@ namespace CTime2.ApplicationModes
         public override void Leave()
         {
             this.Application.Actions.Remove(this._loginHamburgerItem);
+            this.Application.SecondaryActions.Remove(this._switchToTerminalHamburgerItem);
+        }
+
+        private void SwitchToTerminal()
+        {
+            this.Application.CurrentMode = IoC.Get<TerminalApplicationMode>();
         }
     }
 }
---
+++ src/CTime2/ApplicationModes/TerminalApplicationMode.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Caliburn.Micro;
+using CTime2.Views.Terminal;
+using UwCore.Application;
+using UwCore.Hamburger;
+
+namespace CTime2.ApplicationModes
+{
+    public class TerminalApplicationMode : ApplicationMode
+    {
+        private readonly NavigatingHamburgerItem _terminalHamburgerItem;
+        private readonly ClickableHamburgerItem _goBackHamburgerItem;
+
+        public TerminalApplicationMode()
+        {
+            this._terminalHamburgerItem = new NavigatingHamburgerItem("Terminal", Symbol.AllApps, typeof(TerminalViewModel));
+            this._goBackHamburgerItem = new ClickableHamburgerItem("Zur normalen Anmeldung", Symbol.Back, this.GoBack);
+        }
+        
+        public override void Enter()
+        {
+            this.Application.Actions.Add(this._terminalHamburgerItem);
+            this.Application.SecondaryActions.Insert(0, this._goBackHamburgerItem);
+
+            this._terminalHamburgerItem.Execute();
+
+            var appView = ApplicationView.GetForCurrentView();
+            appView.TryEnterFullScreenMode();
+        }
+
+        public override void Leave()
+        {
+            this.Application.Actions.Remove(this._terminalHamburgerItem);
+            this.Application.SecondaryActions.Remove(this._goBackHamburgerItem);
+
+            var appView = ApplicationView.GetForCurrentView();
+            appView.ExitFullScreenMode();
+        }
+
+        private void GoBack()
+        {
+            this.Application.CurrentMode = IoC.Get<LoggedOutApplicationMode>();
+        }
+    }
+}
