afe693586ca1b241049e514ae13ed9e5593d96a4
Daniel Häfele <haefele@xemio.net>
2016-10-22 14:05:46 +0200

    Move the phone and email functionality into their own services

---
+++ src/CTime2.Core/Services/Phone/PhoneService.cs
@@ -0,0 +1,22 @@
+﻿using Windows.ApplicationModel.Calls;
+using Windows.Foundation.Metadata;
+using UwCore.Common;
+
+namespace CTime2.Core.Services.Phone
+{
+    public class PhoneService : IPhoneService
+    {
+        public bool CanCall => ApiInformation.IsApiContractPresent("Windows.ApplicationModel.Calls.CallsPhoneContract", 1);
+
+        public void Call(string phoneNumber, string displayName)
+        {
+            Guard.NotNullOrWhiteSpace(phoneNumber, nameof(phoneNumber));
+            Guard.NotNullOrWhiteSpace(displayName, nameof(displayName));
+
+            if (this.CanCall == false)
+                return;
+
+            PhoneCallManager.ShowPhoneCallUI(phoneNumber, displayName);
+        }
+    }
+}
