78ecc6792a9e53c56ce18c7ec3fbb8a33a79d621
Daniel Häfele <haefele@xemio.net>
2016-10-25 13:15:06 +0200

    Fix how the statistic-diagram is generated for sharing feature
    This is for issue #104.

--- src/CTime2/Views/Statistics/DetailedStatisticView.xaml.cs
+++ src/CTime2/Views/Statistics/DetailedStatisticView.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.IO;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
-using UwCore.Extensions;
+using Windows.Storage;
 
 namespace CTime2.Views.Statistics
 {
@@ -22,12 +22,25 @@ namespace CTime2.Views.Statistics
         {
             var bitmap = new RenderTargetBitmap();
             await bitmap.RenderAsync(this.Content);
+            
+            var file = await ApplicationData.Current.TemporaryFolder.CreateFileAsync("StatisticDiagram.png", CreationCollisionOption.ReplaceExisting);
+            using (var randomAccessStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                var pixels = await bitmap.GetPixelsAsync();
 
-            var pixels = await bitmap.GetPixelsAsync();
-            var randomAccessStream = await pixels.AsStream().ToRandomAccessStreamAsync();
-            var reference = RandomAccessStreamReference.CreateFromStream(randomAccessStream);
-
-            return reference;
+                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, randomAccessStream);
+                encoder.SetPixelData(
+                    BitmapPixelFormat.Bgra8, 
+                    BitmapAlphaMode.Ignore, 
+                    (uint)this.Content.ActualWidth, 
+                    (uint)this.Content.ActualHeight, 
+                    96, 
+                    96,
+                    pixels.ToArray());
+                await encoder.FlushAsync();
+            }
+            
+            return RandomAccessStreamReference.CreateFromFile(file);
         }
     }
 
