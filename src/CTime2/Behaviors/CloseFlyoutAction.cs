e4aa95b54ce70b06b4c175fac9d690551b2bbb40
Daniel Häfele <haefele@xemio.net>
2016-10-28 21:18:44 +0200

    Automatically close the flyout from the save-group app-bar-button

---
+++ src/CTime2/Behaviors/CloseFlyoutAction.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Microsoft.Toolkit.Uwp.UI;
+using Microsoft.Xaml.Interactivity;
+
+namespace CTime2.Behaviors
+{
+    public class CloseFlyoutAction : DependencyObject, IAction
+    {
+        object IAction.Execute(object sender, object parameter)
+        {
+            var element = sender as FrameworkElement;
+            if (element == null)
+                return null;
+
+            var flyoutPresenter = element.FindVisualAscendant<FlyoutPresenter>();
+            if (flyoutPresenter == null)
+                return null;
+
+            var popup = flyoutPresenter.Parent as Popup;
+            if (popup == null)
+                return null;
+
+            popup.IsOpen = false;
+
+            return null;
+        }
+    }
+}
