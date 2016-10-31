df64e2d68cafb677051c35a5faee8650ee6a7469
Daniel Häfele <haefele@xemio.net>
2016-10-31 08:16:19 +0100

    There was an error where it would take already existing times from tomorrow to calculate the current time

--- src/CTime2.Core/Services/CTime/CTimeService.cs
+++ src/CTime2.Core/Services/CTime/CTimeService.cs
@@ -171,7 +171,7 @@ namespace CTime2.Core.Services.CTime
         {
             try
             {
-                IList<Time> timesForToday = await this.GetTimes(employeeGuid, DateTime.Today, DateTime.Today.AddDays(1));
+                IList<Time> timesForToday = await this.GetTimes(employeeGuid, DateTime.Today, DateTime.Today);
 
                 return timesForToday
                     .OrderByDescending(f => f.ClockInTime)
