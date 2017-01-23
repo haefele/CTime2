37c55b2ece80ee113978c030bf8a2b78c918b075
Daniel Häfele <haefele@xemio.net>
2017-01-23 21:01:36 +0100

    Remove the microsoft band integration

--- src/CTime2/Converter/ByteArrayToImageSourceConverter.cs
+++ src/CTime2/Converter/ByteArrayToImageSourceConverter.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
-using UwCore.Extensions;
 using ByteArrayExtensions = CTime2.Extensions.ByteArrayExtensions;
 
 namespace CTime2.Converter
@@ -17,37 +15,6 @@ namespace CTime2.Converter
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
-        }
-    }
-
-    public class FeatureToVisibilityConverter : DependencyObject, IValueConverter
-    {
-        public static readonly DependencyProperty FeatureNameProperty = DependencyProperty.Register(
-            nameof(FeatureName), 
-            typeof(string), 
-            typeof(FeatureToVisibilityConverter), 
-            new PropertyMetadata(default(string)));
-
-        public string FeatureName
-        {
-            get { return (string)this.GetValue(FeatureNameProperty); }
-            set { this.SetValue(FeatureNameProperty, value); }
-        }
-
-
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            var propertyInfo = typeof(Features).GetPropertyCaseInsensitive(this.FeatureName);
-            var isEnabled = (bool)propertyInfo.GetValue(null);
-
-            return isEnabled 
-                ? Visibility.Visible 
-                : Visibility.Collapsed;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotSupportedException();
         }
     }
 }
