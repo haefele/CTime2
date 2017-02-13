f90fe5254a96864bfc10f9491e3430d4187f0d53
Daniel Häfele <haefele@xemio.net>
2017-02-13 23:06:04 +0100

    Add Live-Charts to the statistic diagrams, first test with it

---
+++ src/CTime2/Views/Statistics/StatisticChartItemsToSeriesConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Uwp;
+using ReactiveUI;
+
+namespace CTime2.Views.Statistics
+{
+    public class StatisticChartItemsToSeriesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var chartItems = value as ReactiveList<StatisticChartItem>[];
+
+            if (chartItems == null)
+                return null;
+
+            var dayConfig = Mappers.Xy<StatisticChartItem>()
+                .X(dayModel => (double)dayModel.Date.Ticks / TimeSpan.FromDays(1).Ticks)
+                .Y(dayModel => dayModel.Value);
+
+            var seriesCollection = new SeriesCollection(dayConfig);
+
+            foreach (var line in chartItems)
+            {
+                seriesCollection.Add(new LineSeries
+                {
+                    Values = new ChartValues<StatisticChartItem>(line)
+                });   
+            }
+
+            return seriesCollection;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
