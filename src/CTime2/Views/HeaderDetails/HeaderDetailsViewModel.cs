38cbdb33b7a96f4cbfd880264a96659d9e0919ff
Daniel Häfele <haefele@xemio.net>
2018-10-20 17:56:13 +0200

    Work on moving about and settings to the app header area

--- src/CTime2/Views/HeaderDetails/HeaderDetailsViewModel.cs
+++ src/CTime2/Views/HeaderDetails/HeaderDetailsViewModel.cs
@@ -1,10 +1,15 @@
-﻿using System.Reactive;
+﻿using System;
+using System.Reactive;
 using System.Threading.Tasks;
+using CTime2.ApplicationModes;
+using CTime2.Core.Services.ApplicationState;
 using CTime2.Strings;
 using ReactiveUI;
 using UwCore;
+using UwCore.Application;
 using UwCore.Common;
 using UwCore.Extensions;
+using UwCore.Services.ApplicationState;
 using UwCore.Services.Dialog;
 
 namespace CTime2.Views.HeaderDetails
@@ -12,26 +17,44 @@ namespace CTime2.Views.HeaderDetails
     public class HeaderDetailsViewModel : UwCoreScreen
     {
         private readonly IDialogService _dialogService;
+        private readonly IShell _shell;
+        private readonly IApplicationStateService _applicationStateService;
 
-        public UwCoreCommand<Unit> ShowNoInternetConnection { get; }
+        private readonly ObservableAsPropertyHelper<byte[]> _currentUserImageHelper;
 
-        public HeaderDetailsViewModel(IDialogService dialogService)
+        public byte[] CurrentUserImage => this._currentUserImageHelper.Value;
+
+        public UwCoreCommand<byte[]> RefreshCurrentUserImage { get; }
+
+        public HeaderDetailsViewModel(IDialogService dialogService, IShell shell, IApplicationStateService applicationStateService)
         {
             Guard.NotNull(dialogService, nameof(dialogService));
+            Guard.NotNull(shell, nameof(shell));
+            Guard.NotNull(applicationStateService, nameof(applicationStateService));
             
             this._dialogService = dialogService;
+            this._shell = shell;
+            this._applicationStateService = applicationStateService;
 
-            this.ShowNoInternetConnection = UwCoreCommand
-                .Create(this.ShowNoInternetConnectionImpl)
+            this.RefreshCurrentUserImage = UwCoreCommand.Create(this.RefreshCurrentUserImageImpl)
                 .HandleExceptions();
+            this.RefreshCurrentUserImage.ToProperty(this, f => f.CurrentUserImage, out this._currentUserImageHelper);
+
+            shell.ObservableForProperty(f => f.CurrentMode)
+                .InvokeCommand(this.RefreshCurrentUserImage);
         }
 
-        private Task ShowNoInternetConnectionImpl()
+        private Task<byte[]> RefreshCurrentUserImageImpl()
         {
-            var message = CTime2Resources.Get("NoInternetConnection.Message");
-            var title = CTime2Resources.Get("NoInternetConnection.Title");
+            var user = this._applicationStateService.GetCurrentUser();
+            return Task.FromResult(user?.ImageAsPng);
+        }
 
-            return this._dialogService.ShowAsync(message, title);
+        protected override async void OnInitialize()
+        {
+            base.OnInitialize();
+
+            await this.RefreshCurrentUserImage.ExecuteAsync();
         }
     }
 }
