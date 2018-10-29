05711d1c12a2f84e9ec7209966fb3878b78c47b8
Daniel Häfele <haefele@xemio.net>
2018-10-29 21:02:43 +0100

    Add UserImageButton control for the header area

---
+++ src/CTime2/Controls/UserImageButton.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CTime2.Controls
+{
+    public class UserImageButton : Button
+    {
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(
+            "Image", typeof(byte[]), typeof(UserImageButton), new PropertyMetadata(default(byte[])));
+
+        public byte[] Image
+        {
+            get { return (byte[])this.GetValue(ImageProperty); }
+            set { this.SetValue(ImageProperty, value); }
+        }
+
+        public UserImageButton()
+        {
+            this.DefaultStyleKey = typeof(UserImageButton);
+        }
+    }
+}
