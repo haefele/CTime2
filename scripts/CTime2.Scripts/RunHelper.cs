b3063dc6f9ca4db6d88a4e7a257e35f2c39bc4b5
Daniel Häfele <haefele@xemio.net>
2019-09-08 14:45:22 +0200

    Update build script

--- scripts/CTime2.Scripts/RunHelper.cs
+++ scripts/CTime2.Scripts/RunHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
-using static SimpleExec.Command;
 using System.IO;
+using System.Diagnostics;
 
 namespace CTime2.Scripts
 {
@@ -30,27 +30,27 @@ namespace CTime2.Scripts
 
         public static void RunDotNet(string arguments)
         {
-            Run("dotnet", arguments, noEcho: true);
+            Run("dotnet", arguments);
         }
         public static void RunDotNetTool(string arguments)
         {
-            Run("dotnet", "tool " + arguments, noEcho: true);
+            Run("dotnet", "tool " + arguments);
         }
         public static void RunMsBuild(string arguments)
         {
-            Run(MsBuildPath, arguments, noEcho: true);
+            Run(MsBuildPath, arguments);
         }
         public static void RunGit(string arguments)
         {
-            Run("git", arguments, workingDirectory: CTimePaths.SlnDirectory, noEcho: true);
+            Run("git", arguments, workingDirectory: CTimePaths.SlnDirectory);
         }
         public static void RunNbgv(string arguments)
         {
             try
             {
-                Run(CTimePaths.Nbgv, arguments, workingDirectory: CTimePaths.SlnDirectory, noEcho: true);
+                Run(CTimePaths.Nbgv, arguments, workingDirectory: CTimePaths.SlnDirectory);
             }
-            catch (SimpleExec.NonZeroExitCodeException e) when (e.ExitCode == 5 && arguments == "cloud")
+            catch (Exception e) when (arguments == "cloud")
             {
                 // No cloud environment detected
             }
@@ -61,7 +61,38 @@ namespace CTime2.Scripts
         }
         public static string ReadNbgv(string arguments)
         {
-            return Read(CTimePaths.Nbgv, arguments, workingDirectory: CTimePaths.SlnDirectory, noEcho: true);
+            return Read(CTimePaths.Nbgv, arguments, workingDirectory: CTimePaths.SlnDirectory);
+        }
+
+        public static void Run(string name, string args, string workingDirectory = null)
+        {
+            Read(name, args, workingDirectory);
+        }
+
+        public static string Read(string name, string args, string workingDirectory = null)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = name,
+                    Arguments = args,
+                    WorkingDirectory = workingDirectory,
+                    UseShellExecute = false,
+                    RedirectStandardError = false,
+                    RedirectStandardOutput = true,
+                };
+
+                process.Start();
+
+                var output = process.StandardOutput.ReadToEnd(); //Make sure to read the output before we WaitForExit, or the process might hang forever
+                process.WaitForExit();
+
+                if (process.ExitCode != 0)
+                    throw new Exception($"The command \"{name} {args}\" failed!{Environment.NewLine}{output}");
+
+                return output;
+            }
         }
     }
 }
